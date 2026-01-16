423fd2b14e3241f7c2228de8f9a91509b0b1b845
ADDY-ROBOTICS <adityapandey.robotics@gmail.com>
2026-01-17 00:07:04 +0530

    From today i have started learning c and ill try to stay consistent for a month for this language

---
+++ main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+    //This is my first program !! :) 
+    /*
+    For multiple line comments forward slash and asterisk is used   
+    */
+   printf("I love Robotics!\n");
+   printf("Its really fun.");
+   return 0;
+}
---
+++ variable.c
@@ -0,0 +1,33 @@
+/*#include <stdio.h>
+
+int main(){
+    //variable = container used to store a value 
+    //           Behaves as  if it were the value it contains 
+    int age = 20;
+    int year = 2026;
+    int semester = 2;
+    printf("You are %d years old\n", age);
+    printf("The year is %d\n", year);
+    printf("Now you are in your %dnd semester\n", semester);
+    return 0;
+}*/
+
+/*#include <stdio.h>
+
+int main(){
+    float decimal = 25.5;
+    float temp = 0.21;
+
+    printf("The chances of you waking up early daily throughout the year is %.1f\n ", decimal);
+    printf("The temp is %f temp ", temp);
+    return 0;
+
+}*/
+
+#include <stdio.h>
+
+int main(){
+    char name[]= "ADDY";
+    printf("Hello %s !\n", name);
+    return 0;
+}
